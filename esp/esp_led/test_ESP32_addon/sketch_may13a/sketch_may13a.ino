77daee96176b3e467acb6d0b9ffc687e4c52423c
Ana B <67980649+anablaz@users.noreply.github.com>
2023-05-13 14:44:08 +0200

    Add ESP32 add-on test file

---
+++ esp/esp_led/test_ESP32_addon/sketch_may13a/sketch_may13a.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#define LED 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
+}
