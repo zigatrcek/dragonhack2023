52dca64516a6fd292edd4faa478fafd1c3865928
Ziga Trcek <ziga.trc@gmail.com>
2023-05-14 01:32:39 +0200

    Make basic API interface

---
+++ detection/api.py
@@ -0,0 +1,31 @@
+import requests
+from secret import API_URL
+
+class API:
+    """Class that enables communication with an Azure Function API.
+    """
+    def __init__(self, url):
+        self.url = url
+
+    def get_count(self):
+        response = requests.get(self.url)
+        latest = response.json()[-1]
+        return {
+            'paper': latest['paper'],
+            'container': latest['container'],
+            'other': latest['other']
+        }
+
+    def post_count(self, count):
+        prev_count = self.get_count()
+        count['paper'] += prev_count['paper']
+        count['container'] += prev_count['container']
+        count['other'] += prev_count['other']
+
+        response = requests.post(self.url, json=count)
+        return response.json()
+
+if __name__ == "__main__":
+    api = API(API_URL)
+    print(api.get_count())
+    print(api.post_count({'paper': 0, 'container': 0, 'other': 0}))
